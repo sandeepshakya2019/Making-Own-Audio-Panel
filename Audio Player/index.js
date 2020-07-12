52a80dbcac6e4e3fa1c5f94de3a93d69c523d931
Sandeep Kumar <51978973+sandeepshakya2019@users.noreply.github.com>
2020-07-12 23:24:31 +0530

    Add files via upload

---
+++ Audio Player/index.js
@@ -0,0 +1,108 @@
+// alert("hello")\
+var songs =["Aasan Nahin Yahan","Ab Na Phir Se","chehre me tere khud ko dhundu","Chhod Diya bo rasta jis"];
+
+
+var songTitle = document.getElementById('songTitle');
+var songSlider = document.getElementById('songSlider');
+var currentTime = document.getElementById('currentTime');
+var duration = document.getElementById('duration');
+var volumeSlider = document.getElementById('volumeSlider');
+var nextSongTitle = document.getElementById('nextSongTitle');
+
+var song = new Audio();
+var currentSong = 0;
+
+window.onload = loadSong;
+
+function loadSong () {
+    song.src = "songs/" + songs[currentSong]+".mp3";
+    // console.log(song.src)
+	songTitle.textContent = (currentSong + 1) + ". " + songs[currentSong];
+	nextSongTitle.innerHTML = "<b>Next Song: </b>" + songs[currentSong + 1 % songs.length];
+	song.playbackRate = 1;
+	song.volume = volumeSlider.value;
+	song.play();
+	setTimeout(showDuration, 1000);
+}
+
+setInterval(updateSongSlider, 1000);
+
+function updateSongSlider () {
+    var c = Math.round(song.currentTime);
+    // console.log(c)
+	songSlider.value = c;
+	currentTime.textContent = convertTime(c);
+	if(song.ended){
+		next();
+	}
+}
+
+function convertTime (secs) {
+	var min = Math.floor(secs/60);
+	var sec = secs % 60;
+	min = (min < 10) ? "0" + min : min;
+	sec = (sec < 10) ? "0" + sec : sec;
+	return (min + ":" + sec);
+}
+
+function showDuration () {
+	var d = Math.floor(song.duration);
+	songSlider.setAttribute("max", d);
+	duration.textContent = convertTime(d);
+}
+
+function playOrPauseSong (img) {
+	song.playbackRate = 1;
+	if(song.paused){
+		song.play();
+		img.src = "images/pause.png";
+	}else{
+		song.pause();
+		img.src = "images/play.png";
+	}
+}
+
+function next(){
+    
+    if (currentSong < songs.length - 1){
+        currentSong = currentSong + 1 % songs.length;
+        // console.log(songs.length)
+        loadSong();
+    }
+	
+}
+
+function previous () {
+	currentSong--;
+	currentSong = (currentSong < 0) ? songs.length - 1 : currentSong;
+	loadSong();
+}
+
+function seekSong () {
+	song.currentTime = songSlider.value;
+	currentTime.textContent = convertTime(song.currentTime);
+}
+
+function adjustVolume () {
+    song.volume = volumeSlider.value;
+    console.log(song.volume)
+}
+
+function increasePlaybackRate () {
+    songs.playbackRate += 0.10;
+    
+}
+function mute () {
+    song.volume = 0;
+    volumeSlider.value = 0;
+    // console.log(song.volume)
+}
+function full () {
+    song.volume = 1;
+    volumeSlider.value = 1;
+    // console.log(song.volume)
+}
+
+function decreasePlaybackRate () {
+	songs.playbackRate -= 0.10;
+}
